c1400d048c38799782626293021cf5b4c581f73f
Tom <71088450+TOM1063@users.noreply.github.com>
2022-04-01 12:30:22 +0900

    make multi pages

---
+++ js/wind.js
@@ -0,0 +1,25 @@
+var capture;
+var w = windowWidth;
+var h = windowWidth*9/16;
+
+function setup() {
+    capture = createCapture({
+        audio: false,
+        video: {
+            width: w,
+            height: h
+        }
+    }, function() {
+        console.log('capture ready.')
+    });
+    capture.elt.setAttribute('playsinline', '');
+    capture.hide();
+    capture.size(windowWidth, windowWidth*9/16);
+    canvas = createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    //capture.resize(255,255);
+    image(capture, 0, 0);
+
+}
