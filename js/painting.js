b61cc64264fb13b2ecd5cd09514eb777c7e2be68
Tom <71088450+TOM1063@users.noreply.github.com>
2022-04-07 23:30:27 +0900

    multiple take

--- js/painting.js
+++ js/painting.js
@@ -14,7 +14,9 @@ var rect_size_x;
 var rect_size_y;
 var margin;
 var current_line_id;
-var debug = false;
+var debug = true;
+
+var image_num;
 
 
 function preload(){
@@ -22,7 +24,8 @@ function preload(){
         img = loadImage("js/map.png");    //画像の読み込み
         img_withedge = loadImage("js/map_withedge.png");
         //img_edge = loadImage("js/map_edge.png");
-        img_edge = loadImage("js/building2_edge.png");
+        img_edge_1 = loadImage("js/building1_edge.png");
+        img_edge_2 = loadImage("js/building2_dge.png");
     }
 }
 function setup() {
@@ -34,36 +37,51 @@ function setup() {
     current_line_id = 0;
     points_x[current_line_id] = [];
     points_y[current_line_id] = [];
+    image_num = 1;
     createCanvas(windowWidth,windowHeight);
     //background(70);
     button_0 = createButton('FIND VIEW');
     button_0.position(windowWidth/2 -100 , windowHeight/2 + windowHeight/6 );
     button_0.mousePressed(gotoscene1);
     button_0.size(250,70);
-    let col = color(255);
-    button_0.style('background-color',col);
+    let col_0 = color(255);
+    button_0.style('background-color',col_0);
     button_0.style('font-size', '25px');
+
     time = 0;
     //fill(255,255,210);
-    fill(255,255,255);
-    noStroke();
-    //rect(20,80,windowWidth-40,windowHeight - 200);
-    noFill();
-    stroke(0);
-    strokeWeight(1);
-    rect(windowWidth/2 - rect_size_x/2,windowHeight/2 - rect_size_y/2-windowHeight/12,rect_size_x,rect_size_y);
     
 }
 
 
 
 function mousePressed(){
+    console.log("mousePressed");
     points_x[current_line_id] = [];
     points_y[current_line_id] = [];
     console.log("current_line_id changed to" + str(current_line_id));
+    mouse_x = mouseX;
+    mouse_y = mouseY;
+    area_x = 300;
+    area_y = 200;
+    if (scene_num == 0) {
+        points_x[current_line_id].push(mouse_x);
+        points_y[current_line_id].push(mouse_y);
+    }
+    else if(scene_num == 2) {
+        var is_on_area_x = (windowWidth/2 - area_x/2 < mouse_x) && (mouse_x < windowWidth/2 + area_x/2);
+        var is_on_area_y = (windowHeight-area_y < mouse_y) && (mouse_y < windowHeight);
+        var is_on_area = is_on_area_x && is_on_area_y;
+        if(is_on_area) {
+            console.log("hidden_button_is_clicked");
+            gotoscene0();
+        }
+
+    }
 }
 
 function mouseDragged() {
+    console.log("mouseDragged");
     mouse_x = mouseX;
     mouse_y = mouseY;
     if (scene_num == 0) {
@@ -71,7 +89,9 @@ function mouseDragged() {
         points_y[current_line_id].push(mouse_y);
     }
 }
+
 function mouseReleased(){
+    console.log("mouseReleased");
     current_line_id += 1;
 }
 
@@ -99,6 +119,24 @@ function gotoscene1() {
 function gotoscene2() {
     scene_num = 2;
 }
+function gotoscene0() {
+    console.log("gotoscene0_setting");
+    points_x.length = 0;
+    points_y.length = 0;
+    current_line_id = 0;
+    points_x[current_line_id] = [];
+    points_y[current_line_id] = [];
+    console.log("lines_initialized");
+    background(255);
+    console.log("gotoscene0");
+    if(image_num = 1){
+        image_num = 2;
+    }
+    else if(image_num = 2) {
+        image_num = 1;
+    }
+    scene_num = 0;
+}
 
 function scene0() {
     // fill(255);
@@ -107,6 +145,16 @@ function scene0() {
     // textFont("Sawarabi Mincho");
     // textAlign(CENTER, CENTER);
     // text('ダレデモ画伯', windowWidth/2, 40);
+    button_0.position(windowWidth/2 -100 , windowHeight/2 + windowHeight/6 );
+
+
+    fill(255,255,255);
+    noStroke();
+    //rect(20,80,windowWidth-40,windowHeight - 200);
+    noFill();
+    stroke(0);
+    strokeWeight(1);
+    rect(windowWidth/2 - rect_size_x/2,windowHeight/2 - rect_size_y/2-windowHeight/12,rect_size_x,rect_size_y);
 
     fill(0, 102, 153);
     noFill();
@@ -158,6 +206,7 @@ function scene2() {
     textAlign(CENTER, CENTER);
     text('Your Drawing', windowWidth/2 - rect_size_x/2 + 125,windowHeight/2 - margin/2 - rect_size_y + 25);
 
+
     noFill();
     stroke(0);
     strokeWeight(1.9);
@@ -190,8 +239,14 @@ function scene2() {
     }
 
     if(debug != true){
-        img_edge.resize(rect_size_x,rect_size_y);
-        image(img_edge,windowWidth/2 - rect_size_x/2,windowHeight/2 + margin/2);
+        if(image_num == 1){
+            img_edge_1.resize(rect_size_x,rect_size_y);
+            image(img_edge_1,windowWidth/2 - rect_size_x/2,windowHeight/2 + margin/2);
+        }
+        else if(image_num == 2){
+            img_edge_2.resize(rect_size_x,rect_size_y);
+            image(img_edge_2,windowWidth/2 - rect_size_x/2,windowHeight/2 + margin/2);
+        }
     }
 
     fill(0);
