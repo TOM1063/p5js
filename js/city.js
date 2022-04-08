ba20b7cd9935b3ec25e429e52ef515e0e464977d
Tom <71088450+TOM1063@users.noreply.github.com>
2022-04-08 11:19:04 +0900

    modelview

---
+++ js/city.js
@@ -0,0 +1,21 @@
+let obj_model;
+
+
+function setup() {
+    obj_model = loadModel('assets/bldg.obj');
+    createCanvas(windowWidth, windowHeight/2, WEBGL);
+    
+
+}
+
+function draw() {
+    background(0);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    model(obj_model);
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+    //sphere(300);
+
+}
