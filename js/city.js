07e27ae74dd73b1bf95c33acb4b54ba71b66f2c9
Tom <71088450+TOM1063@users.noreply.github.com>
2022-04-08 13:56:25 +0900

    design

--- js/city.js
+++ js/city.js
@@ -18,8 +18,9 @@ function draw() {
     rotateX(frameCount * 0.005);
     rotateY(frameCount * 0.005);
     noFill();
+    fill(255,255,255,10);
     stroke(255);
-    strokeWeight(2);
+    strokeWeight(1);
     //sphere(300);
     model(obj_model);
     console.log("model_show");
