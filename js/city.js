ec313eb6e0c7b7070cd0b2adad30653717dd1a62
Tom <71088450+TOM1063@users.noreply.github.com>
2022-04-09 05:23:14 +0900

    style

--- js/city.js
+++ js/city.js
@@ -17,11 +17,12 @@ function setup() {
 function draw() {
     background(0);
     //rotateX(frameCount * 0.005);
-    rotateX(3.14*45/180);
+    rotateX(3.14*60/180);
     rotateZ(frameCount * 0.002);
     noFill();
     stroke(255,255,255,10);
-    strokeWeight(1);
+    strokeWeight(0.5);
+    scale(2);
     //sphere(300);
     model(obj_model);
     //
